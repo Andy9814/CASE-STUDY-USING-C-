685cce6ea019a30184fb59a24f947892d12b67fd
Andy9814 <nripdeep98@gmail.com>
2017-09-28 09:41:46 -0400

    Finished Lab 5b

--- CaseStudyTests/EmployeeModelTests.cs
+++ CaseStudyTests/EmployeeModelTests.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
 using HelpdeskDAL ;
-namespace CaseStudyTests
+namespace CaseStudyTests 
 {
     [TestClass]
     public class EmployeeModelTests
@@ -22,7 +22,70 @@ namespace CaseStudyTests
             Employee someEmployee = model.GetByEmial("nripdeep@");
                 Assert.IsNull(someEmployee);
         }
+
+        [TestMethod]
+        public void EmployeeModelGetAllShouldReturnList()
+        {
+            EmployeeModel model = new EmployeeModel();
+            List<Employee> allEmployees = model.GetAll();
+            Assert.IsTrue(allEmployees.Count > 0);
+        }
+
+        [TestMethod]
+        public void EmployeeModelAddShouldReturnNewId()
+        {
+            EmployeeModel model = new EmployeeModel();
+            Employee newEmployee = new Employee();
+            newEmployee.Title = "Mr.";
+            newEmployee.FirstName = "Test";
+            newEmployee.LastName = "Student";
+            newEmployee.Email = "ts@abc.com";
+            newEmployee.PhoneNo = "(555)555-5551";
+            newEmployee.DepartmentId = 100;
+            int newId = model.Add(newEmployee);
+            Assert.IsTrue(newId > 0);
+
+        }
+
+        [TestMethod]
+        public void EmployeeModelGetByIdShouldReturnList()
+        {
+            EmployeeModel model = new EmployeeModel();
+
+            Employee someEmployee = model.GetByEmial("ts@abc.com");
+            Employee anotherEmployee = model.GetById(someEmployee.Id);
+
+            Assert.IsNotNull(anotherEmployee);
+        }
+
+
+        [TestMethod]
+        public void EmployeeModelUpdateShouldReturnNewId()
+        {
+            EmployeeModel model = new EmployeeModel();
+            Employee updateEmployee = model.GetByEmial("ts@abc.com");
+            updateEmployee.Email = "ts@xyz.com";
+
+            int EmployeeUpdated = model.Update(updateEmployee);
+            Assert.IsTrue(EmployeeUpdated > 0);
+
+        }
+
+
+        public void EmployeeModelDeleteShouldReturnNewId()
+        {
+            EmployeeModel model = new EmployeeModel();
+            Employee deleteEmployee = model.GetByEmial("ts@xyz.com");
+
+            int EmployeeDeleted = model.delete(deleteEmployee.Id);
+            Assert.IsTrue(EmployeeDeleted == 1);
+
+        }
+
+
+
+
     }
 
-    
+
 }
--- HelpdeskDAL/EmployeeModel.cs
+++ HelpdeskDAL/EmployeeModel.cs
@@ -26,5 +26,124 @@ namespace HelpdeskDAL
             return selectedEmployee;
 
 
-        } }
+        }
+
+        public Employee GetById(int id)
+        {
+            Employee selectedEmployees = null;
+            try
+            {
+                HelpdeskContext ctx = new HelpdeskContext();
+                selectedEmployees = ctx.Employees.FirstOrDefault(Emp => Emp.Id == id);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+
+            }
+            return selectedEmployees;
+        }
+
+
+        public List<Employee> GetAll()
+        {
+            List<Employee> allEmployees = new List<Employee>();
+            try
+            {
+                HelpdeskContext ctx = new HelpdeskContext();
+                allEmployees = ctx.Employees.ToList();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+
+            }
+            return allEmployees;
+        }
+
+
+
+
+
+
+        public int Add(Employee newEmployee)
+        {
+            try
+            {
+                HelpdeskContext ctx = new HelpdeskContext();
+
+                ctx.Employees.Add(newEmployee);
+                ctx.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+
+            }
+            return newEmployee.Id;
+        }
+
+
+
+
+
+        public int Update(Employee updateEmployee)
+        {
+            int EmployeesUpdated = -1;
+            try
+            {
+                HelpdeskContext ctx = new HelpdeskContext();
+                Employee currentEmployee = ctx.Employees.FirstOrDefault(stu => stu.Id == updateEmployee.Id);
+                ctx.Entry(currentEmployee).CurrentValues.SetValues(updateEmployee);
+
+                EmployeesUpdated = ctx.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+
+            }
+            return EmployeesUpdated;
+
+        }
+
+
+
+        public int delete(int id)
+        {
+            int EmployeesDeleted = -1;
+            try
+            {
+                HelpdeskContext ctx = new HelpdeskContext();
+                Employee currentStudent = ctx.Employees.FirstOrDefault(Emp => Emp.Id == id);
+                ctx.Employees.Remove(currentStudent);
+
+
+                EmployeesDeleted = ctx.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod().Name + " " + ex.Message);
+                throw ex;
+
+            }
+            return EmployeesDeleted;
+        }
+
+
+
+
+
+
+
+    }
 }
