2b2b9bfd127e9798bccc3c33d5e66bf7bfbbf153
Andy9814 <nripdeep98@gmail.com>
2017-10-09 06:02:44 -0400

    L-7

--- HelpDeskWebsite/Scripts/employeeLookUp.js
+++ HelpDeskWebsite/Scripts/employeeLookUp.js
@@ -1,1 +1,51 @@
-﻿
+﻿$(function () {
+    $('#getbutton').click(function (e) {
+        var lastname = $('#TextBoxLastname').val();
+        $('#status').text('please wait....');
+        ajaxCall('Get', 'api/employee/' + lastname, ' ')
+            .done(function (data)// actually see ajax.done();
+        {
+            if (data.Lastname !== 'not found')
+            {
+                $('#email').text(data.Email);
+                $('#title').text(data.Title);
+                $('#firstname').text(data.Firstname);
+                $('#phone').text(data.Phoneno);
+                $('#status').text('employee found');
+            } else
+            {
+                $('#firstname').text('not found');
+                $('#email').text('');
+                $('#title').text('');
+                $('#phone').text('');
+                $('#status').text('no such Employee');
+            }
+        })
+            .fail(function (jqXHR, textStatus, errorThrown) 
+            {
+                errorRoutine(jqXHR);
+            });
+    });
+    function ajaxCall(type, url, data) {
+        return $.ajax({// every ajax returns a promise 
+           // method: type,//pst,Get,put,delete
+            type: type,
+            url: url,
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            processData: true
+        });
+    }
+    function errorRoutine(jqXHR)
+    {
+        if (jqXHR.responseJSON == null)
+        {
+            $('#status').text(jqXHR.responseText);
+        }
+        else
+        {
+            $('#status').text(jqXHR.responseJSON.Message);
+        }
+    }
+});
